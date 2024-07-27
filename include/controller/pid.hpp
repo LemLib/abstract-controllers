4dbe21a80bbf7f407cec59f94f3f6791044d2fc0
Aang099 <avatarred13@gmail.com>
2024-07-27 16:55:37 -0700

    PID config field documentation

--- include/controller/pid.hpp
+++ include/controller/pid.hpp
@@ -5,45 +5,89 @@
 
 namespace controllers {
 template <isQuantity In, isQuantity Out> struct pidConfig {
-        double kP, kI = 0, kD = 0, iCut = 1;
-        In iMax = In(0.0), inBase = In(1.0), maxThreshold = In(0.0);
-        Out outBase = Out(1.0), outMax = Out(0.0);
+  double kP, kI = 0, kD = 0; // Constants for Proportional, Integral, Derivative
+
+  /**
+   * @brief Integral is multiplied by this amount if sign of error changes (you
+   * can safely ignore if kI is 0)
+   */
+  double integralCut = 1;
+
+  /**
+   * @brief The maximum absolute value of the integral.
+   */
+  In integralMax = In(0.0);
+
+  /**
+   * @brief The highest absolute value that can be outputted.
+   */
+  Out outMax = Out(0.0);
+
+  /**
+   * @brief If the absolute value of the error is above this number, use
+   * bang-bang instead of PID. Set to NaN to disable.
+   */
+  In bbangThreshold = In(0.0);
+
+  /**
+   * @brief The value to output when bang-bang is active, multiplied by the sign
+   * of the error.
+   */
+  Out bbangValue = Out(0.0);
+
+  /**
+   * @brief The base unit to use for the input (effectively scales constants)
+   */
+  In inBase = In(1.0);
+
+  /**
+   * @brief The base unit to use for the output (effectively scales constants)
+   */
+  Out outBase = Out(1.0);
 };
 
-template <isQuantity In, isQuantity Out> class PID : public Controller<In, In, Out> {
-        pidConfig<In, Out>& m_config;
-        double m_integral = 0, m_prev_error = INFINITY;
-        Out m_current;
-    public:
-        PID(pidConfig<In, Out>& config)
-            : m_config(config) {}
+template <isQuantity In, isQuantity Out>
+class PID : public Controller<In, In, Out> {
+  pidConfig<In, Out> &m_config;
+  double m_integral = 0, m_prev_error = NAN;
+  Out m_current;
 
-      
+public:
+  PID(pidConfig<In, Out> &config) : m_config(config) {}
 
-        virtual Out update(const In &input) override { // todo: test this all
-            double error = (this->target - input).convert(m_config.inBase);
-            if (m_prev_error == INFINITY) m_prev_error = error;
-            double threshold = m_config.maxThreshold.convert(m_config.inBase);
-            double max = m_config.outMax.convert(m_config.outBase);
-            if ((threshold != 0) && (max != 0) && (error > threshold)) {
-                m_current = units::copysign(m_config.outMax, error); // bang bang
-            } else {
-                if ((error < 0) != (m_prev_error < 0)) m_integral *= m_config.iCut; // cut down I if the sign of the error changes
-                else if ((m_config.iMax != In(0.0)) && (m_integral >= m_config.iMax.convert(m_config.inBase)))
-                    m_integral = std::copysign(m_config.iMax.convert(m_config.inBase), m_integral); // cap I
-                else m_integral += error; 
-                double output = (error * m_config.kP) + (m_integral * m_config.kI) + ((m_prev_error - error) * m_config.kD);
-                m_prev_error = error;
-                if (fabs(output) > max) output = std::copysign(max, error); // cap output
-                m_current = output * m_config.outBase;
-            }
-            return m_current;
-        }
+  virtual Out update(const In &input) override { // todo: test this all
+    double error = (this->target - input).convert(m_config.inBase);
+    if (std::isnan(m_prev_error))
+      m_prev_error = error;
+    double threshold = m_config.bbangThreshold.convert(m_config.inBase);
+    double max = m_config.outMax.convert(m_config.outBase);
+    if ((threshold != 0) && (max != 0) && (error > threshold)) {
+      m_current = units::copysign(m_config.bbangValue, error); // bang bang
+    } else {
+      if ((error < 0) != (m_prev_error < 0))
+        m_integral *=
+            m_config.integralCut; // cut down I if the sign of the error changes
+      else if ((m_config.integralMax != In(0.0)) &&
+               (m_integral >= m_config.integralMax.convert(m_config.inBase)))
+        m_integral =
+            std::copysign(m_config.integralMax.convert(m_config.inBase),
+                          m_integral); // cap I
+      else
+        m_integral += error;
+      double output = (error * m_config.kP) + (m_integral * m_config.kI) +
+                      ((m_prev_error - error) * m_config.kD);
+      m_prev_error = error;
+      if (fabs(output) > max)
+        output = std::copysign(max, error); // cap output
+      m_current = output * m_config.outBase;
+    }
+    return m_current;
+  }
 
-        virtual void reset() {
-            m_integral = 0;
-            m_prev_error = INFINITY;
-            m_current = Out(0);
-        }
+  virtual void reset() {
+    m_integral = 0;
+    m_prev_error = NAN;
+    m_current = Out(0);
+  }
 };
-} // namespace jam
+} // namespace controllers
